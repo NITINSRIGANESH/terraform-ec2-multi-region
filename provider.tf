84999942c383f56154504a5cf6080dbced0092b3
NITINSRIGANESH <116146765+NITINSRIGANESH@users.noreply.github.com>
2025-01-03 23:01:05 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,27 +1,9 @@
-# Provider for US-East-1
 provider "aws" {
-  region  = "us-east-1"
-  profile = "us-east-1"
+  region = var.region_1
 }
 
-resource "aws_instance" "example_us_east_1" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Replace with a valid AMI ID for the US-East-1 region
-  instance_type = "t2.micro"
-  tags = {
-    Name = "US-East-1-Instance"
-  }
-}
-
-# Provider for Tokyo (AP-Northeast-1)
 provider "aws" {
-  region  = "ap-northeast-1"
-  profile = "ap-northeast-1"
+  alias  = "second_region"
+  region = var.region_2
 }
 
-resource "aws_instance" "example_ap_northeast_1" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Replace with a valid AMI ID for the AP-Northeast-1 region
-  instance_type = "t2.micro"
-  tags = {
-    Name = "Tokyo-Instance"
-  }
-}
