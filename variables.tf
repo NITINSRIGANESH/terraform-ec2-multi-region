8f2301195d0c8191f039c2009c656c5a2d1eb45a
NITINSRIGANESH <116146765+NITINSRIGANESH@users.noreply.github.com>
2025-01-03 23:01:34 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,9 @@
 variable "region_1" {
   description = "First AWS region"
-  default     = "us-east-1"  # You can change this to any other AWS region
+  default     = "us-east-1"
 }
 
 variable "region_2" {
   description = "Second AWS region"
-  default     = "us-west-2"  # You can change this to any other AWS region
+  default     = "us-west-2"
 }
-
